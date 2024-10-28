d32229cbb2d66d072da9b9304515860285c89ee3
joohe71 <joohe71@gmail.com>
2024-10-28 18:30:32 +0900

    chore: react-query 라이브러리 추가 및 최상단 파일 내부QueryClientProvider  적용

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -3,12 +3,16 @@ import { theme } from "@/styles/theme";
 import { MantineProvider } from "@mantine/core";
 import { globalStyle } from "@/styles/global";
 import { Global } from "@emotion/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
   return (
-    <MantineProvider withCSSVariables theme={theme}>
-      <Global styles={globalStyle} />
-      <Component {...pageProps} />
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider withCSSVariables theme={theme}>
+        <Global styles={globalStyle} />
+        <Component {...pageProps} />
+      </MantineProvider>
+    </QueryClientProvider>
   );
 }
